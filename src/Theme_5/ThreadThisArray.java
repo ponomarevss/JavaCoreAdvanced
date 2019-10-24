fbc477c4da480578ee31e67f9538ff1a0c1d2e65
ponomarevss <s-siberia@yandex.ru>
2019-10-24 14:21:16 +0500

    исправлены: - ошибка в математическом выражении - добавлено смещение индекса для расчета значений массива в цикле - обединение потоков выполняется до склейки массивов - добавлена проверка эквивалентности конечных массивов

--- src/Theme_5/ThreadThisArray.java
+++ src/Theme_5/ThreadThisArray.java
@@ -1,57 +1,76 @@
 package Theme_5;
 
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+import java.util.Arrays;
 
 public class ThreadThisArray {
-    static final int SIZE = 10000000;
+    static final int SIZE = 10_000_000;
     static final int HALF = SIZE / 2;
 
-    private static float[] fillIt(float[] arr) {
+    private static float[] createArray(int size) {
+        float[] arr = new float[size];
+        Arrays.fill(arr, 1.0f);
+        return arr;
+    }
+
+    private static float[] calcIt(float[] arr, int indexShift) {
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1;
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + (i + indexShift) / 5) * Math.cos(0.2f + (i + indexShift) / 5) * Math.cos(0.4f + (i + indexShift) / 2));
         }
         return arr;
     }
 
-    private static float[] calcIt(float[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + 1 / 2));
-        }
-        return arr;
+    private static void method1(float[] arr) {
+        long a = System.currentTimeMillis();
+        calcIt(arr, 0);
+        System.out.println(System.currentTimeMillis() - a + " consequent method\t");
     }
 
-    private static void method1() {
-        float[] arr = new float[SIZE];
-        fillIt(arr);
-        long a = System.currentTimeMillis();
-        calcIt(arr);
-        System.out.println(System.currentTimeMillis() - a);
-    }
-
-    private static void method2() {
-        float[] arr = new float[SIZE];
-        fillIt(arr);
-        long a = System.currentTimeMillis();
+    private static void method2(float[] arr) {
         float[] a1 = new float[HALF];
         float[] a2 = new float[HALF];
 
+        long a = System.currentTimeMillis();
         System.arraycopy(arr, 0, a1, 0, HALF);
         System.arraycopy(arr, HALF, a2, 0, HALF);
 
-        new Thread(() -> calcIt(a1)).start();
-        new Thread(() -> calcIt(a2)).start();
+//        Thread thread1 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                calcIt(a1, 0);
+//            }
+//        });
+//        thread1.start();
+//        Thread thread2 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                calcIt(a2, HALF);
+//            }
+//        });
+//        thread2.start();
+
+        Thread thread1 = new Thread(() -> calcIt(a1, 0));
+        Thread thread2 = new Thread(() -> calcIt(a2, HALF));
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         System.arraycopy(a1, 0, arr, 0, HALF);
         System.arraycopy(a2, 0, arr, HALF, HALF);
-/*
-        calcIt(a1);
-        calcIt(a2);
-*/
-        System.out.println(System.currentTimeMillis() - a);
+
+        System.out.println(System.currentTimeMillis() - a + " parallel method\t");
     }
 
     public static void main(String[] args) {
-        method2();
-        method1();
+        float[] array1 = createArray(SIZE);
+        method1(array1);
+        float[] array2 = createArray(SIZE);
+        method2(array2);
+        System.out.println("arrays equals: " + Arrays.equals(array1, array2));
     }
 }
